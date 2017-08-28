5ea99c0196ffd2d63a30bbe6475efed3febbed01
Paulo Ragonha <paulo@ragonha.me>
2017-08-28 16:44:59 +0200

    The fire 🔥

--- src/slides/Introduction.js
+++ src/slides/Introduction.js
@@ -3,14 +3,10 @@ import React from 'react'
 export default () =>
   <section>
     <section>
-      <h1>side-effects as a function of state</h1>
-    </section>
-    <section>
-      <h2>who are we?</h2>
-
+      <h2>The least painful way to build a web app</h2>
       <ul>
-        <li>Fernando Via Canel @xaviervia</li>
-        <li>Paulo Ragonha @pirelenito</li>
+        <li>Fernando - @xaviervia</li>
+        <li>Paulo - @pirelenito</li>
       </ul>
     </section>
   </section>
